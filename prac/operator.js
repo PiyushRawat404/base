1693ab07cb668caaaf726ad0f8a8a824fa42e870
PiyushRawat404 <piyush.rawat@marblex.ai>
2026-01-27 15:20:31 +0530

    oprators are added

---
+++ prac/operator.js
@@ -0,0 +1,47 @@
+// //assignment operators
+// let a= 10;
+// let b = 20;
+
+// a+=5;
+// console.log(a)
+// b-=5;
+// console.log("value of b :"+b)
+// a*=2
+// console.log(a)
+// a/=2
+// console.log(a)
+// b%=2
+// console.log(b)
+// a**=2
+// console.log(a)
+
+// //equality operators
+
+// let i="5"
+// let j=5
+// console.log(i==j)
+// console.log(i===j)
+
+// console.log(i!=j)
+// console.log(i!==j)
+
+// //logical operators and comparison operators
+
+// function check(a){
+//     if(a>10 && a<12){
+//         console.log("value"+a)
+//     }
+//     if(a%2==0||a<=15){
+//         console.log(`value :${a} `)
+//     }
+// }
+// check(20)
+
+//bitwise operators
+console.log(5 & 1)
+console.log(5 | 1)
+console.log(5 ^ 1)
+console.log( ~5 )
+console.log(5 << 1)
+console.log(5 >> 1)
+
