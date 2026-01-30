be3ee0688a953329b2441e029a96e020f4ce3220
Piyush <piyush@WILASSETA0145.local>
2026-01-30 17:47:08 +0530

    Remove ignored files

--- dist/controllers/user.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-declare const getUser: (req: Request, res: Response) => Promise<void>;
-declare const newUser: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export { getUser, newUser };
-//# sourceMappingURL=user.d.ts.map
--- dist/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_1 = require("../controllers/user");
-const router = (0, express_1.Router)();
-router.get("/:name", user_1.getUser);
-router.post("/register", user_1.newUser);
-exports.default = router;
-//# sourceMappingURL=user.routes.js.map
