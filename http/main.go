f376f6f3a4e40720649283ee5acb16088a27be4b
Himanshu_Thakur <himanshu.thakur@marblex.ai>
2026-05-18 18:45:55 +0530

    Postgres connection with go app

--- http/main.go
+++ http/main.go
@@ -1,58 +1,58 @@
-package main
+// package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-)
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"net/http"
+// )
 
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
+// type User struct {
+// 	ID   int    `json:"id"`
+// 	Name string `json:"name"`
+// 	Age  int    `json:"age"`
+// }
 
 
-func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+// func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
 
-		fmt.Println("Request:", r.Method, r.URL.Path)
+// 		fmt.Println("Request:", r.Method, r.URL.Path)
 
-		next(w, r)
-	}
-}
+// 		next(w, r)
+// 	}
+// }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to server")
-}
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "Welcome to server")
+// }
 
-func getUser(w http.ResponseWriter, r *http.Request) {
+// func getUser(w http.ResponseWriter, r *http.Request) {
 
-	user := User{
-		ID:   1,
-		Name: "Himanshu",
-		Age:  22,
-	}
+// 	user := User{
+// 		ID:   1,
+// 		Name: "Himanshu",
+// 		Age:  22,
+// 	}
 
-	json.NewEncoder(w).Encode(user)
-}
+// 	json.NewEncoder(w).Encode(user)
+// }
 
-func createUser(w http.ResponseWriter, r *http.Request) {
+// func createUser(w http.ResponseWriter, r *http.Request) {
 
-	var user User
+// 	var user User
 
-	json.NewDecoder(r.Body).Decode(&user)
+// 	json.NewDecoder(r.Body).Decode(&user)
 
-	fmt.Fprintf(w, "User created: %s", user.Name)
-}
+// 	fmt.Fprintf(w, "User created: %s", user.Name)
+// }
 
-func main() {
+// func main() {
 
-	http.HandleFunc("/", loggingMiddleware(homeHandler))
-	http.HandleFunc("/user", loggingMiddleware(getUser))
-	http.HandleFunc("/create-user", loggingMiddleware(createUser))
+// 	http.HandleFunc("/", loggingMiddleware(homeHandler))
+// 	http.HandleFunc("/user", loggingMiddleware(getUser))
+// 	http.HandleFunc("/create-user", loggingMiddleware(createUser))
 
-	fmt.Println("Server running on port 8080")
+// 	fmt.Println("Server running on port 8080")
 
-	http.ListenAndServe(":8080", nil)
-}
+// 	http.ListenAndServe(":8080", nil)
+// }
--- http/main2.go
+++ http/main2.go
@@ -1,18 +1,27 @@
-// package main
+package main
 
-// import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
-// func greet(ch chan string) {
-// 	ch <- "Hello from goroutine!" // Send value to channel
-// }
+func main() {
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+	defer cancel()
+	go slowOperation(ctx)
+	time.Sleep(3 * time.Second)
+	fmt.Println("MAIN DONE")
+}
+func slowOperation(ctx context.Context) {
+	select {
+	case <-time.After(3 * time.Second):
+		fmt.Println("OPERATION COMPLETED")
 
-// func main() {
-
-// 	messageChannel := make(chan string)
-
-// 	go greet(messageChannel)
-
-// 	msg := <-messageChannel
-
-// 	fmt.Println(msg)
-// }
+	case <-ctx.Done():
+		fmt.Println("Operation canceled", ctx.Err())
+	}
+}
