ef2b95dc45b17f4abc0097cfc4e343c60af54f1c
PiyushRawat404 <piyush.rawat@marbleai.x>
2026-05-20 20:27:45 +0530

    refactor:schema loding

--- blog/internal/db/db.go
+++ blog/internal/db/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"blog/internal/config"
 
@@ -12,11 +13,24 @@ import (
 func LoadDB(cfg config.Config) (*pgx.Conn, error) {
 
 	dsn := "postgres://" + cfg.DBUser + ":" + cfg.DBPassword + "@" + cfg.DBHost + ":" + cfg.DBPort + "/" + cfg.DBName
+
 	conn, err := pgx.Connect(
 		context.Background(),
 		dsn,
 	)
 	if err != nil {
+		return nil, err
+	}
+
+	schema, err := os.ReadFile("migrations/schema.up.sql")
+	if err != nil {
+		conn.Close(context.Background())
+		return nil, err
+	}
+
+	_, err = conn.Exec(context.Background(), string(schema))
+	if err != nil {
+		conn.Close(context.Background())
 		return nil, err
 	}
 
--- blog/internal/routes/routes.go
+++ blog/internal/routes/routes.go
@@ -8,8 +8,8 @@ import (
 func Routes(handler *handler.Handler) {
 
 	http.HandleFunc("/", handler.Static)
-	http.HandleFunc("/blogs/create", handler.CreateBlog)
-	http.HandleFunc("/blogs", handler.GetBlogs)
+	http.HandleFunc("/blogs", handler.CreateBlog)
+	http.HandleFunc("/blog", handler.GetBlogs)
 	http.HandleFunc("/blogs/update", handler.UpdateBlog)
 	http.HandleFunc("/blogs/delete", handler.DeleteBlog)
 }
